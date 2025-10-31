fa60b97d48da62410709364e6e42e1bd8b6386c6
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-10-31 18:08:55 +0900

    ~5 method

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -46,6 +46,22 @@ impl Rectangle { //Rectangle 구조체에 대한 implementation 블록..
     fn width(&self) -> bool {
         self.width > 0
     }
+
+    fn can_hold(&self, other: &Rectangle) -> String {
+        if self.width > other.width && self.height > other.height {
+            String::from("yes")
+        } else {
+            String::from("no")
+        }
+    }
+
+    //첫 인자가 Self가 아니므로 메소드는 아닌 연관함수. 
+    fn square(size: u32) -> Self { //Self는 impl 블록 대상이 되는 타입의 별칭!!!! 여기선 Rectangle
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -88,6 +104,26 @@ fn main() {
     if rect1.width() {
         println!("The rectangle has a nonzero width; it is {}", rect1.width);
     }
+
+    //매개변수 많은 메소드
+    let rect_1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect_2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect_3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect_1 hold rect_2? {}", rect_1.can_hold(&rect_2));
+    println!("Can rect_1 hold rect_3? {}", rect_1.can_hold(&rect_3));
+
+    let sq=Rectangle::square(3);
+    println!("Square is {:#?}", sq);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
