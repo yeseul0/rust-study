5a35ba1c6c1679e55cc634a1d9e7a1668820f198
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-11-03 18:31:34 +0900

    ~7.3 pub 전

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -22,3 +22,17 @@ mod front_of_house {
 //          ├── take_order
 //          ├── serve_order
 //          └── take_payment
+
+//front_of_house 공개 API
+pub fn eat_at_restaurant() {
+    //absolute path : crate 루트부터 시작. (file system이랑 비슷))
+    crate::front_of_house::hosting::add_to_waitlist();
+    // -> eat_at_waitlist()만 이동시에는 절대경로가 유리
+
+    //relative path : 작성되고 있는 현재 모듈 기준. self, super 식별 사용 가능
+    front_of_house::hosting::add_to_waitlist();
+    // -> front_of_house , eat_at_restaurant() 같이 이동시에 상대경로가 유리
+
+    //hosting이 private이라 compile error
+
+}
