4f946390208d9898f134c55e9402a421b4bb05b3
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-11-03 17:52:11 +0900

    crate = module tree root

---
+++ projects/restaurant/src/lib.rs
@@ -0,0 +1,24 @@
+//front_of_house 모듈 내에 hosting, serving 모듈 nested
+mod front_of_house {
+    mod hosting {
+        fn add_to_waitlist() {}
+        fn seat_at_table() {}
+    }
+
+    mod serving {
+        fn take_order() {}
+        fn serve_order() {}
+        fn take_payment() {}
+    }
+}
+
+// module tree
+// crate (아 crate가 module tree의 루트가 되는구나.)
+//  └── front_of_house
+//      ├── hosting
+//      │   ├── add_to_waitlist
+//      │   └── seat_at_table
+//      └── serving
+//          ├── take_order
+//          ├── serve_order
+//          └── take_payment
