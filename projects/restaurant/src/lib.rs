81ef6104037044f6634a8b77e89d3818f82ee418
yeseul0 <choiyeseul@g.hongik.ac.kr>
2025-11-04 18:45:18 +0900

    ~7.3 pub

--- projects/restaurant/src/lib.rs
+++ projects/restaurant/src/lib.rs
@@ -1,7 +1,9 @@
 //front_of_house 모듈 내에 hosting, serving 모듈 nested
+fn deliver_order() {}
+
 mod front_of_house {
-    mod hosting {
-        fn add_to_waitlist() {}
+    pub mod hosting {
+        pub fn add_to_waitlist() {}
         fn seat_at_table() {}
     }
 
@@ -9,6 +11,35 @@ mod front_of_house {
         fn take_order() {}
         fn serve_order() {}
         fn take_payment() {}
+    }
+
+    mod back_of_house {
+        fn fix_incorrect_order() {
+            cook_order();
+            super::deliver_order(); //여기서 super모듈은 crate임.
+        }
+
+        fn cook_order() {}
+
+        pub struct Breakfast {
+            pub toast: String,
+            seosonal_fruit: String,
+        }
+        impl Breakfast {
+            //Breakfast 구조체는 seosonal_fruit 비공필드 있어서, 꼭 비공필드 값 넣어주는 연관함수가 필요함!!
+            // (없으면 객체 생성 불가!)
+            pub fn summer(toast: &str) -> Breakfast {
+                Breakfast{
+                    toast: String::from(toast),
+                    seasonal_fruit: String::from("peaches"),
+                }
+            }
+        }
+
+        pub enum Appetizer {
+            Soup,
+            Salad,
+        }
     }
 }
 
@@ -33,6 +64,17 @@ pub fn eat_at_restaurant() {
     front_of_house::hosting::add_to_waitlist();
     // -> front_of_house , eat_at_restaurant() 같이 이동시에 상대경로가 유리
 
-    //hosting이 private이라 compile error
+    //hosting이 private이라 compile error -> pub mod
+    //근데 hosting 내부 함수도 private임. -> pub fn
+    //pub는 상위 모듈이 해당 모듈을 가리킬 수 있도록만 함. 내부 구현은 따로 열어둬야함
 
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    meal.toast = String::from("Wheat");
+    println!("i'd like {} toast please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+    
 }
+
